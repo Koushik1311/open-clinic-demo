3bcaba06d46ffcb074699cb8d201351e438b2e77
Koushik <programmer.koushik@gmail.com>
2024-11-25 19:54:04 +0530

    feat: developed hero section

--- app/(base)/(home)/page.tsx
+++ app/(base)/(home)/page.tsx
@@ -1,11 +1,39 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-center min-h-screen text-4xl font-extrabold">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-      </p>
-    </div>
+    <main>
+      {/* Hero Section */}
+      <section>
+        {/* Heading */}
+        <h1 className="text-center text-5xl font-bold md:text-6xl mb-6 text-stone-900 leading-[4rem] md:leading-[5rem]">
+          Your Complete Clinic <br className="md:inline hidden" />
+          with AI
+        </h1>
+        {/* Subheading */}
+        <div className="max-w-2xl mx-auto">
+          <p className="text-center text-lg text-stone-700 mb-8">
+            Transform patient care as AI handles consultations and analysis,
+            doctors focus on final decisions
+          </p>
+        </div>
+
+        {/* Sign up button and Learn more button */}
+        <div className="max-w-sm md:max-w-none mx-16 md:mx-auto flex flex-col md:flex-row items-center justify-center gap-4">
+          <Link
+            href="/sign-up/doctor"
+            className="border border-orange-500 bg-orange-500 w-full md:w-auto py-2 px-3 text-sm font-medium rounded-lg"
+          >
+            Doctor Sign up
+          </Link>
+          <Link
+            href="/patient-sign-up"
+            className="border border-orange-500 w-full md:w-auto py-2 px-3 text-sm font-medium rounded-lg"
+          >
+            Patient Sign up
+          </Link>
+        </div>
+      </section>
+    </main>
   );
 }
---
+++ app/sign-up/layout.tsx
@@ -0,0 +1,5 @@
+export default function SignUpLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return <div>{children}</div>;
+}
